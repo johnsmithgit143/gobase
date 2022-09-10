f5b29e432ae569af1f8413c71cc723e2111d6cf5
johnsmithgit143 <johnsmithgit143@github.com>
2022-09-10 09:53:05 +0800

    testing flag

--- cat/go.mod
+++ cat/go.mod
@@ -2,4 +2,4 @@ module github.com/johnsmithgit143/gobase/cat
 
 go 1.19
 
-require github.com/johnsmithgit143/gobase/include v0.0.0-20220909050630-cc933501a784
+require github.com/johnsmithgit143/gobase/include v0.0.0-20220909051204-0508eb18aea4
--- flag/flag.go
+++ flag/flag.go
@@ -1,5 +1,54 @@
-package flag
+package main
 
 import (
-	
+	"os"
+	"fmt"
+	"strings"
 )
+
+var Validflags = [...]string{
+	"-a", "-b", "-c", "--testing",
+}
+
+func Parse() (map[string]int, error) {
+	var (
+		flags = make(map[string]int)
+		i int
+		x int
+	)
+	start:
+	for i = 1; i < len(os.Args); i++ {
+		if os.Args[i] == "--" {
+			break
+		}
+		if !strings.HasPrefix(os.Args[i], "-") {
+			continue
+		}
+		for x = 0; x < len(Validflags); x++ {
+			if os.Args[i] == Validflags[x] {
+				flags[os.Args[i]] = i
+				continue start
+			}
+		}
+		return flags, fmt.Errorf("invalid option: %s", os.Args[i])
+	}
+	return flags, nil
+}
+
+func foo() {
+	START:
+	for y := 0; y < 5; y++ {
+		fmt.Println("y:", y)
+		for x := 0; x < 5; x++ {
+			if x == 4 {
+				continue START
+			}
+			fmt.Println("x:", x)
+		}
+	}
+}
+
+func main() {
+	foo()
+	fmt.Println(Parse())
+}
